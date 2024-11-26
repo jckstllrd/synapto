ec299943e9f495eac62d3c2e5939855c3da6d924
jckstllrd <jackstallard17@gmail.com>
2024-11-26 18:10:23 +0000

    Setup and create template for project

---
+++ webpack.dev.js
@@ -0,0 +1,27 @@
+const { merge } = require("webpack-merge");
+const common = require("./webpack.common.js");
+
+module.exports = merge(common, {
+  mode: "development",
+  devtool: "inline-source-map",
+  devServer: {
+    watchFiles: ["./src/template.html"],
+    hot: true,
+    client: {
+      overlay: false,
+    },
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+    ],
+  },
+});
